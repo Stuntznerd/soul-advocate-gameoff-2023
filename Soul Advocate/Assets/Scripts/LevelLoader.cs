e98aae2a42ff69da0ab8dc587631e4c8bd2184bc
Joshua Stuntzner <joshua.stuntzner@gmail.com>
2023-12-03 16:56:11 -0600

    Solved level loader problem
    previous level's instance of level loader was still subscribed to "onlevelcomplete" solution was to unsubscribe it before loading next level. Going to investigate similar issue with audio manager

--- Soul Advocate/Assets/Scripts/LevelLoader.cs
+++ Soul Advocate/Assets/Scripts/LevelLoader.cs
@@ -12,21 +12,30 @@ public class LevelLoader : MonoBehaviour
         // StartCoroutine(TestLoad(SceneManager.GetActiveScene().buildIndex + 1));
         GameManager.OnLevelComplete += LoadNextLevel;
         // StartGame.OnStartButtonPressed += LoadNextLevel;
+        Debug.Log("Level Loader: " + this.GetInstanceID().ToString());
         Debug.Log("Loaded Scenes: " + SceneManager.loadedSceneCount.ToString());
         Debug.Log("Scenes: " + SceneManager.sceneCount.ToString());
     }
 
     public void LoadNextLevel() {
-        // StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
-        StartCoroutine(AsyncLoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        GameManager.OnLevelComplete -= LoadNextLevel;
+        Debug.Log("Before start coroutine");
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        Debug.Log("After coroutine");
+        // StartCoroutine(AsyncLoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
     
     IEnumerator LoadLevel(int levelIndex) {
+        Debug.Log("1");
         transition.SetTrigger("Start");
 
+        Debug.Log("2");
         yield return new WaitForSeconds(transTime);
 
+        Debug.Log("3");
         SceneManager.LoadScene(levelIndex);
+
+        Debug.Log("4");
     }
 
     // IEnumerator TestLoad(int levelIndex) {
@@ -39,16 +48,16 @@ public class LevelLoader : MonoBehaviour
     //     SceneManager.LoadScene(levelIndex);
     // }
 
-    IEnumerator AsyncLoadLevel(int levelIndex) {
-        transition.SetTrigger("Start");
+    // IEnumerator AsyncLoadLevel(int levelIndex) {
+    //     transition.SetTrigger("Start");
 
-        yield return new WaitForSeconds(transTime);
+    //     yield return new WaitForSeconds(transTime);
 
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelIndex);
+    //     AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelIndex);
 
-        while (!asyncLoad.isDone)
-        {
-            yield return null;
-        }
-    }
+    //     while (!asyncLoad.isDone)
+    //     {
+    //         yield return null;
+    //     }
+    // }
 }
